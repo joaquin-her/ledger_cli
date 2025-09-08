6f9787ed5c94e29643cd6dd67eba6462e6eb113b
Joaquin H <hernandez.joaquin346@gmail.com>
2025-09-08 17:17:49 -0300

    print currencies raw

--- ledger_app/lib/ledger_cli.ex
+++ ledger_app/lib/ledger_cli.ex
@@ -5,11 +5,34 @@ defmodule LedgerApp do
 
 
   def read_transactions(path) do
-    case File.read(path) do
+    path
+    |> File.stream!()
+    |> CSV.decode!([separator: ?;,headers: true])
+    |> Enum.each(fn row ->
+      row
+      |>IO.puts()
+    end)
+  end
 
-      {:ok, content} -> IO.puts(content)
+  def read_currencies(path) do
+    currencies =
+      path
+      |> File.stream!()
+      |> CSV.decode!(separator: ?;, headers: true)
+      |> Enum.map(fn row ->
+        %{
+          moneda: row["nombre_moneda"],
+          valor: row["precio_usd"]
+        }
+      end)
+      |> Enum.to_list()
 
-      {:error, :enoent} -> IO.puts("Error: El archivo #{path} no fue encontrado")
-    end
+    Enum.each(currencies, fn currency ->
+      print_currency(currency.moneda, currency.valor)
+    end)
+  end
+
+  defp print_currency(name, value) do
+    IO.puts("#{name} : #{value} USD")
   end
 end
