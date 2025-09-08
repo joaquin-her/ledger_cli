55e07307deb5df3f8d2a9649fb3c9aed94dbc4ec
Joaquin H <hernandez.joaquin346@gmail.com>
2025-09-08 16:13:17 -0300

    imprimiendo transacciones en crudo

--- ledger_app/lib/ledger_cli.ex
+++ ledger_app/lib/ledger_cli.ex
@@ -2,4 +2,14 @@ defmodule LedgerApp do
   @moduledoc """
   Documentation for `LedgerApp`.
   """
+
+
+  def read_transactions(path) do
+    case File.read(path) do
+
+      {:ok, content} -> IO.puts(content)
+
+      {:error, :enoent} -> IO.puts("Error: El archivo #{path} no fue encontrado")
+    end
+  end
 end
