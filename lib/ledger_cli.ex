2b71369d0c0dd18100167fc248b416af1e54f602
Joaquin H <hernandez.joaquin346@gmail.com>
2025-09-08 18:03:40 -0300

    return Transacciones

--- lib/ledger_cli.ex
+++ lib/ledger_cli.ex
@@ -3,15 +3,19 @@ defmodule LedgerApp do
   Documentation for `LedgerApp`.
   """
 
-
+  alias Transaccion
   def read_transactions(path) do
-    path
+    transacciones = path
     |> File.stream!()
     |> CSV.decode!([separator: ?;,headers: true])
-    |> Enum.each(fn row ->
-      row
-      |> IO.inspect()
+    |> Enum.map(fn row ->
+      %Transaccion{
+        id: row["id_transaccion"] |> String.to_integer(),
+        timestamp: row["timestamp"] |> String.to_integer(),
+        tipo: row["tipo"] |> String.to_atom()
+      }
     end)
+    transacciones
   end
 
   def read_currencies(path) do
