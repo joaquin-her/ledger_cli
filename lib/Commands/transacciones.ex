6c1c52c87c8dd78b808271ad5cceb8db9a2e40d4
Joaquin H <hernandez.joaquin346@gmail.com>
2025-09-11 15:25:10 -0300

    implemented filter by destiny account

--- lib/Commands/transacciones.ex
+++ lib/Commands/transacciones.ex
@@ -16,4 +16,20 @@ defmodule Commands.TransactionsCommand do
         |> Enum.filter(fn transaction -> transaction.cuenta_origen == account_name end)
     end
   end
+
+  def get_transactions_of_account(transactions, account_name) do
+    transactions
+    |> filter_by_origin_account(account_name)
+    |> Enum.concat(filter_by_destiny_account( transactions, account_name))
+    |> Enum.sort_by(fn t -> t.timestamp end)
+  end
+
+  def filter_by_destiny_account(transactions, account_name) do
+    case account_name do
+      "all" -> transactions
+      _ ->
+        transactions
+        |> Enum.filter(fn transaction -> transaction.cuenta_destino == account_name end)
+    end
+  end
 end
