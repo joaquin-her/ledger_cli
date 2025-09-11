930f1692d37aa6495b7f93c1fda6f172fba13606
Joaquin H <hernandez.joaquin346@gmail.com>
2025-09-11 15:29:27 -0300

    some csv helper functions jic

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,14 @@
 ExUnit.start()
+# Helper functions para los tests
+defmodule TestHelpers do
+  def create_temp_csv(content, filename \\ "temp_test.csv") do
+    File.write!(filename, content)
+    filename
+  end
+
+  def cleanup_temp_file(filename) do
+    if File.exists?(filename) do
+      File.rm!(filename)
+    end
+  end
+end
