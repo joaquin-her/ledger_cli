a5a4d6698ce9d24d50e0505838e2fda828415b46
Joaquin H <hernandez.joaquin346@gmail.com>
2025-09-11 10:09:53 -0300

    added escritp to build an executable

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule LedgerApp.MixProject do
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-
+      escript: escripts(),
       # Docs
       name: "ledger",
       docs: &docs/0
@@ -26,13 +26,17 @@ defmodule LedgerApp.MixProject do
   def deps do
   [
     {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true},
-    {:csv, "~> 2.4" }
+    {:csv, "~> 2.4" },
   ]
+  end
+
+  defp escripts do
+    [main_module: LedgerApp.CLI]
   end
 
   defp docs do
     [
-      main: LedgerApp,
+      main: LedgerApp.CLI,
       extras: ["README.md"]
     ]
   end
