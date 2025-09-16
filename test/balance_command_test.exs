57aca3dd9efd267ce44afa9987f11976d43da863
Joaquin H <hernandez.joaquin346@gmail.com>
2025-09-16 12:20:49 -0300

    test error, <nro line> to balance command when hay varias altas

--- test/balance_command_test.exs
+++ test/balance_command_test.exs
@@ -8,7 +8,7 @@ defmodule BalanceCommandTests do
       %Transaccion{id: 1, timestamp: "1754937004", moneda_origen: "USDT", moneda_destino: "", monto: "5000.0", cuenta_origen: "userA", cuenta_destino: "", tipo: :alta_cuenta},
     ]
     assert %{ USDT: 5000.0 } == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userA", moneda: "all"})
-  end  
+  end
 
   test "la suma de 3 transferencias positivas genera un balance correcto" do
     transactions = [
@@ -24,7 +24,7 @@ defmodule BalanceCommandTests do
     transactions = [
       %Transaccion{id: 1, timestamp: "1754937001", moneda_origen: "USDT", moneda_destino: "", monto: "5000.0", cuenta_origen: "userA", cuenta_destino: "", tipo: :alta_cuenta},
       %Transaccion{id: 2, timestamp: "1754937002", moneda_origen: "ETH", moneda_destino: "", monto: "50.0", cuenta_origen: "userB", cuenta_destino: "", tipo: :alta_cuenta},
-          
+
       %Transaccion{id: 1, timestamp: "1754937004", moneda_origen: "USDT", moneda_destino: "", monto: "1000.0", cuenta_origen: "userA", cuenta_destino: "userB", tipo: :transferencia},
       %Transaccion{id: 3, timestamp: "1754937002", moneda_origen: "USDT", moneda_destino: "", monto: "1000.0", cuenta_origen: "userA", cuenta_destino: "userB", tipo: :transferencia},
       %Transaccion{id: 4, timestamp: "1754937002", moneda_origen: "USDT", moneda_destino: "", monto: "1500.0", cuenta_origen: "userA", cuenta_destino: "userB", tipo: :transferencia},
@@ -46,20 +46,20 @@ defmodule BalanceCommandTests do
   end
   test "no tener como objetivo una cuenta especifica debe arrojar una excepcion" do
       assert true
-  end  
+  end
 
   test "si la cuenta buscada no esta dentro de los datos debe devolver un :error y la causa de por que es un error" do
       assert true
-  end  
+  end
 
 
-  test "la aparicion de varias alta_cuenta de una misma cuenta debe arrojar un error" do
+  test "la aparicion de varias alta_cuenta de una misma cuenta debe arrojar un error con la linea donde fue encontrado" do
     transactions = [
       %Transaccion{id: 1, timestamp: "1754937004", moneda_origen: "USDT", moneda_destino: "", monto: "5000.0", cuenta_origen: "userA", cuenta_destino: "", tipo: :alta_cuenta},
       %Transaccion{id: 2, timestamp: "1754937024", moneda_origen: "USDT", moneda_destino: "", monto: "6000.0", cuenta_origen: "userA", cuenta_destino: "", tipo: :alta_cuenta},
       %Transaccion{id: 3, timestamp: "1755541804", moneda_origen: "USDT", moneda_destino: "", monto: "150.0", cuenta_origen: "userA", cuenta_destino: "userB", tipo: :transferencia},
       %Transaccion{id: 4, timestamp: "1745541804", moneda_origen: "ETH", moneda_destino: "", monto: "0.0001214", cuenta_origen: "userB", cuenta_destino: "userA", tipo: :transferencia}
     ]
-    assert %{ USDT: 3850.0, ETH: 0.0001214 } == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userA", moneda: "all"})
-  end  
+    assert { :error, 2} == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userA", moneda: "all"})
+  end
 end
