6eb09ba019885a6b759e53ad6a51c2a3642cd964
Joaquin <hernandez.joaquin346@gmail.com>
2025-09-17 12:28:39 -0300

    changed expected output

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -5,20 +5,20 @@ defmodule CliTest do
   test "print in console all transactions" do
     expected_output =
 "ID | TIMESTAMP | MONEDA_ORIGEN | MONEDA_DESTINO | MONTO | CUENTA_ORIGEN | CUENTA_DESTINO | TIPO\n" <>
-" 1 | 1754937004 | USDT | USDT | 100.50 | userA | userB | transferencia\n" <>
-" 2 | 1755541804 | BTC | USDT | 0.1 | userB |  | swap\n" <>
-" 3 | 1756751404 | BTC |  | 50000 | userC |  | alta_cuenta\n" <>
-" 4 | 1757183404 | USDT | BTC | 500.25 | userD | userE | transferencia\n" <>
-" 5 | 1757615404 | ETH | USDT | 1.5 | userF | userG | swap\n"
+"1;1754937004;USDT;USDT;100.50;userA;userB;transferencia;\n" <>
+"2;1755541804;BTC;USDT;0.1;userB;;swap\n" <>
+"3;1756751404;BTC;;50000;userC;;alta_cuenta\n" <>
+"4;1757183404;USDT;BTC;500.25;userD;userE;transferencia\n" <>
+"5;1757615404;ETH;USDT;1.5;userF;userG;swap\n"
     assert expected_output == capture_io(fn ->
-      LedgerApp.CLI.run_command("transacciones -t test_data.csv")
+      LedgerApp.CLI.run_command(["transacciones", "-t", "test_data.csv"])
     end)
   end
 
   test "usar el comando balance sin el flag -c1 debe arrojar un error" do
     expected_output = "Error: Debe especificar una cuenta origen con -c1\n"
     assert expected_output == capture_io(fn ->
-      LedgerApp.CLI.run_command("balance -t test_data.csv")
+      LedgerApp.CLI.run_command(["balance", "-t", "test_data.csv"])
     end)
   end
 
