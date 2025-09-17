bc0c0908003e55fd5040b5d809ee610c842619be
Joaquin <hernandez.joaquin346@gmail.com>
2025-09-17 12:35:26 -0300

    changed to return tuples

--- test/balance_command_test.exs
+++ test/balance_command_test.exs
@@ -8,7 +8,7 @@ defmodule BalanceCommandTests do
       %Transaccion{id: 1, timestamp: "1754937004", moneda_origen: "USDT", moneda_destino: "", monto: "5000.0", cuenta_origen: "userA", cuenta_destino: "", tipo: :alta_cuenta},
     ]
     conversion_values = %{}
-    assert %{ USDT: 5000.0 } == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userA", moneda: "all"}, conversion_values)
+    assert { :ok, %{ USDT: 5000.0 }} == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userA", moneda: "all"}, conversion_values)
   end
 
   test "la suma de 3 transferencias positivas genera un balance correcto" do
@@ -20,7 +20,7 @@ defmodule BalanceCommandTests do
       %Transaccion{id: 4, timestamp: "1754937002", moneda_origen: "USDT", moneda_destino: "", monto: "1000.0", cuenta_origen: "userA", cuenta_destino: "userB", tipo: :transferencia},
     ]
     conversion_values = %{}
-    assert %{ USDT: 2000.0, BTC: 5.0 } == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userB", moneda: "all"}, conversion_values)
+    assert { :ok, %{ USDT: 2000.0, BTC: 5.0 }} == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userB", moneda: "all"}, conversion_values)
   end
   test "la suma de 3 transferencias y un gasto genera un balance correcto" do
     transactions = [
@@ -33,7 +33,7 @@ defmodule BalanceCommandTests do
       %Transaccion{id: 5, timestamp: "1745541804", moneda_origen: "ETH", moneda_destino: "", monto: "0.1", cuenta_origen: "userB", cuenta_destino: "userA", tipo: :transferencia}
     ]
     conversion_values = %{}
-    assert %{ USDT: 3500.0, ETH: 49.9 } == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userB", moneda: "all"} , conversion_values)
+    assert { :ok, %{ USDT: 3500.0, ETH: 49.9 }} == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userB", moneda: "all"} , conversion_values)
   end
   test "la suma de 3 transferencias positivas y una negativa en una cantidad muy pequeña  genera un balance correcto" do
     transactions = [
@@ -46,7 +46,7 @@ defmodule BalanceCommandTests do
       %Transaccion{id: 4, timestamp: "1745541806", moneda_origen: "BTC", moneda_destino: "", monto: "0.0001214", cuenta_origen: "userB", cuenta_destino: "userA", tipo: :transferencia}
     ]
     conversion_values = %{}
-    assert %{BTC: 2700.9998786 } == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userB", moneda: "all"}, conversion_values)
+    assert %{ :ok,{BTC: 2700.9998786 }} == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userB", moneda: "all"}, conversion_values)
   end
 
 
@@ -67,7 +67,7 @@ defmodule BalanceCommandTests do
       %Transaccion{id: 2, timestamp: "1754937024", moneda_origen: "USDT", moneda_destino: "ETH", monto: "5000.0", cuenta_origen: "userA", cuenta_destino: "", tipo: :swap},
     ]
     conversion_values = %{"BTC" => 67500.00, "ETH" => 2500.00, "USDT" => 1.00}
-    assert %{ USDT: 0.0, ETH: 2.0 } == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userA", moneda: "all"}, conversion_values)
+    assert {:ok, %{ USDT: 0.0, ETH: 2.0 }} == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userA", moneda: "all"}, conversion_values)
   end
 
   test "un swap con un monto mayor al disponible arroja un error " do
@@ -89,6 +89,6 @@ defmodule BalanceCommandTests do
       %Transaccion{id: 4, timestamp: "1745541806", moneda_origen: "BTC", moneda_destino: "", monto: "0.0001214", cuenta_origen: "userB", cuenta_destino: "userA", tipo: :transferencia}
     ]
     conversion_values = %{"BTC" => 30000.00, "ETH" => 2500.00, "USDT" => 1.00}
-    assert %{USDT: 81029996.358 } == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userB", moneda: "USDT"}, conversion_values)
+    assert { :ok, %{USDT: 81029996.358 }} == BalanceCommand.get_balance(transactions, %{cuenta_origen: "userB", moneda: "USDT"}, conversion_values)
   end
 end
