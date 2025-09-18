8e7476924c3be24f7d9c18483bf5491369fe0fd1
Joaquin H <hernandez.joaquin346@gmail.com>
2025-09-18 16:44:48 -0300

    removed deprecrated func

--- lib/database/transaccion.ex
+++ lib/database/transaccion.ex
@@ -2,10 +2,6 @@ defmodule Transaccion do
   @enforce_keys [:id, :tipo, :cuenta_origen, :timestamp]
   defstruct [:id, :timestamp, :moneda_origen, :moneda_destino, :monto, :cuenta_origen, :cuenta_destino, :tipo]
 
-  def get_keys() do
-    [ :id, :timestamp, :moneda_origen, :moneda_destino, :monto, :cuenta_origen, :cuenta_destino, :tipo ]
-  end
-
   defimpl String.Chars do
 
     def to_string(%Transaccion{id: id, timestamp: timestamp, moneda_origen: moneda_origen, moneda_destino: moneda_destino, monto: monto, cuenta_origen: cuenta_origen, cuenta_destino: cuenta_destino, tipo: tipo}) do
