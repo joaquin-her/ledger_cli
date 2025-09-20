bd8e21fe5a1fef498e90a39b7130770022d94d30
Joaquin H <hernandez.joaquin346@gmail.com>
2025-09-20 03:47:29 -0300

    update ledger version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LedgerApp.MixProject do
   def project do
     [
       app: :ledger,
-      version: "0.1.9",
+      version: "0.2.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
